c8b86fe56561f484a0d412e1233ec4d8a2990001
Atichat Lappanopakon <boss_atln@outlook.com>
2019-07-24 19:29:41 +0700

    Try to set prop visisble but not working

--- src/my-map.js
+++ src/my-map.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { DeckGL } from '@deck.gl/react'
 import { StaticMap } from 'react-map-gl'
-import { GridCellLayer } from '@deck.gl/layers'
+import { GridCellLayer, ScatterplotLayer } from '@deck.gl/layers'
 import MAPBOX_TOKEN from './constant'
 import LayerManager from './LayerManager'
 
@@ -15,29 +15,63 @@ const initialViewState = {
   bearing: 0
 };
 
-const data =[{point: [-122.41669, 37.7853], value: 100}, {point: [-122.42669, 37.7853], value: 50}]
-
 const gridCellLayer = new GridCellLayer({
   id: 'grid-cell-layer',
-  data,
+  data: [{point: [-122.41669, 37.7853], value: 100}, {point: [-122.42669, 37.7853], value: 50}],
+  visible: true,
   pickable: true,
   extruded: false,
   cellSize: 250,
   getPosition: d => d.point,
   getFillColor: d => [48, 128, d.value * 255, 255],
-});
+})
 
-const layers = [gridCellLayer]
+const scatterplotLayer = new ScatterplotLayer({
+  id: 'scatterplot-layer',
+  data: [
+    {name: 'Colma', passengers: 200, coordinates: [-122.466233, 37.684638]},
+    {name: 'Civic Center', passengers: 500, coordinates: [-122.413756,37.779528]},
+  ],
+  visible: false,
+  pickable: true,
+  opacity: 1,
+  stroked: true,
+  filled: true,
+  radiusScale: 10,
+  radiusMinPixels: 1,
+  radiusMaxPixels: 100,
+  lineWidthMinPixels: 1,
+  getPosition: d => d.coordinates,
+  getRadius: d => Math.sqrt(d.passengers),
+  getFillColor: d => [255, 140, 0],
+  getLineColor: d => [0, 0, 0],
+})
 
-const getLayerOptions = () => {
+const LAYERS = [gridCellLayer, scatterplotLayer]
+
+const getLayerOptions = (layers=[]) => {
+  console.log(layers)
   return layers.map(layer => {
-    return { id: layer.id, label: layer.id.split('-').join(" ")}
+    return { id: layer.id, label: layer.id, visible: layer.props.visible }
   })
+}
+
+const renderLayers = renderIds => {
+  const layers = LAYERS.filter(layer => renderIds.find(r => r.id === layer.id).visible)
+  console.log(layers[0].props)
+  // layers[0].setProps({visible: false})
+  return layers
 }
 
 class MyMap extends Component {
   state = {
     mapStyle: "mapbox://styles/mapbox/light-v9",
+    renderIds: []
+  }
+
+  componentDidMount() {
+    const renderIds = getLayerOptions(LAYERS)
+    this.setState(() => ({renderIds}))
   }
 
   handleLoadStaticMap = () => {
@@ -57,14 +91,14 @@ class MyMap extends Component {
   }
 
   handleUpdateLayer = newOption => {
-    const newLayers = layers.filter(layer => layer.id === newOption.id ? newOption.value : true)
-    this.renderDeckLayer(newLayers)
+    // const newLayers = layers.filter(layer => layer.id === newOption.id ? newOption.value : true)
+    // this.renderDeckLayer(newLayers)
   }
 
-  renderDeckLayer = (layers = []) => {
-    const { deck } = this.deckGl
-    deck.setProps({layers})
-  }
+  // renderDeckLayer = (layers = []) => {
+  //   const { deck } = this.deckGl
+  //   deck.setProps({layers})
+  // }
 
   handleChangeMapStyle = styleId => {
     const { staticMap } = this
@@ -74,7 +108,7 @@ class MyMap extends Component {
   }
 
   render() {
-    const { mapStyle } = this.state
+    const { mapStyle, renderIds } = this.state
 
     return (
       <Fragment>
@@ -82,12 +116,13 @@ class MyMap extends Component {
             _updateConfig={this.handleUpdateConfig} 
             _changeMapStyle={this.handleChangeMapStyle}
             _updateLayer={this.handleUpdateLayer}
-            layerOptions={getLayerOptions()}>
+            layerOptions={getLayerOptions(LAYERS)}>
         </LayerManager>
         <DeckGL
           ref={ref => { this.deckGl = ref }}
           initialViewState={initialViewState}
           controller={true}
+          layers={renderIds.length !== 0 && renderLayers(renderIds)}
         >
           <StaticMap
             ref={ref => { this.staticMap = ref && ref.getMap() }}
