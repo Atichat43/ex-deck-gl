bd62ae42531bfdd71f6e2def87f85978fa8bdff5
Atichat Lappanopakon <boss_atln@outlook.com>
2019-07-25 10:09:37 +0700

    Test setting git config username/email

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import MyMap from './my-map'
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Map"><MyMap></MyMap></div>
       </header>
+        <div className="Map">
+          <MyMap></MyMap>
+        </div>
     </div>
   );
 }
